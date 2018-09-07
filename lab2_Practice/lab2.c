b9c459f4b78456ea0a5f7886d7e0cf968779836d
root <root@HP-LAPTOP.localdomain>
2018-09-07 09:56:28 -0700

    lab graded

--- lab2_Practice/lab2.c
+++ lab2_Practice/lab2.c
@@ -2,6 +2,29 @@
 	Author: Max Novak
 	Date: 9/1/2018
 	CS 1 Lab 2 - Practice
+	Pseudo Code
+	Phase 1: 
+		- int, long int, unsigned int, chararacter, float, double, long double  
+	Phase 2:
+		- int int_var;
+		- long int long_int_var;
+		- unsigned int u_int_var;
+		- char char_var;
+		- float float_var;
+		- double double_var;
+		- long double long_double_var;
+	Phase 3:
+		I. Declare variables
+		II. Print the values to console
+		III. end the program with no error code
+	Phase 4:
+		I.
+			a. initilalize with values, declare with specified data types, and set meaningful identifiers
+		II.
+			a. print using printf
+			b. format the output
+		III.
+			a. return with no error code
 */
 
 //allow input/output stream manipulation
@@ -19,8 +42,8 @@ int main()
 	long double long_double_var = 4.123091230912309;
 
 	//print format specified variables to console
-	printf("%i\n%d\n%u\n%c\n%f\n%f\n%Lf\n", int_var, long_int_var, u_int_var, char_var, float_var, double_var, long_double_var);
+	printf("%i\n%ld\n%u\n%c\n%.2f\n%.4f\n%.20Lf\n", int_var, long_int_var, u_int_var, char_var, float_var, double_var, long_double_var);
 
 	//return no error code on successful execution
-	exit(0);
+	return 0;
 }
