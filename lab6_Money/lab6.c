ba2f95675fb6205cf91543a79f7c864c8f22c942
MaxNova-CompSci <maxnova.compsci@gmail.com>
2018-09-23 01:24:59 -0700

    completed and commented lab 6

--- lab6_Money/lab6.c
+++ lab6_Money/lab6.c
@@ -25,7 +25,7 @@
 				* accnt_name, customer_name, init_balance,
 				* debts, payments, and credit_limit
 		II. 
-			a. new_balance = init_balance-debts+payments;
+			a. new_balance = init_balance+debts-payments;
 			b. if(new_balance > credit_limit)
 				* print(-1)
 		III.
@@ -34,10 +34,13 @@
 				* and credit remaining
 */
 
+//include input and ouput function library
 #include <stdio.h>
 
 int main()
 {
+	//declare required variables
+	//you never know when hyper inflation hits so the data is very high precision
 	long int accnt_num;
 	char customer_name[50];
 	long double init_balance;
@@ -47,26 +50,44 @@ int main()
 	long double new_balance;
 	long double credit_remaining;
 
+	//Input series that asks the user to input data and that of which is stored
+	//Some information is identifying them and the other set is for calculating values
 	puts("Please enter account number");
-	fgets(stdin, "%d", &accnt_num);
+	scanf("%ld", &accnt_num);
+	getchar();
+ 	
+ 	/*
+	~~~Thing to note~~~
+		- scanf leaves \n in buffer so get char is required to remove before using gets afterwards
+	*/
 
 	puts("Please enter customer name");
-	fgets(stdin, "%s", &customer_name);
+	gets(customer_name);
 
 	puts("Please enter your opening balance");
-	fgets(stdin, "%Lf", &init_balance);
+	scanf("%Lf", &init_balance);
 
 	puts("Please enter the charges on your card");
-	fgets(stdin, "%Lf", &debts);
+	scanf("%Lf", &debts);
 
 	puts("Please enter the payments on your card");
-	fgets(stdin, "%Lf", &payments);
+	scanf("%Lf", &payments);
 
 	puts("Please enter your maximum credit limit");
-	fgets(stdin, "%Lf", &credit_limit);
+	scanf("%Lf", &credit_limit);
 
-	new_balance = init_balance-debts+payments;
+	//Preform calculations on the data
+	//value is determined in persepective from the bank
+	new_balance = init_balance+debts-payments;
+	//credit remaining is the credit they may use next month
+	credit_remaining = credit_limit-new_balance;
 
+	//Output formatted data to the user
+	printf("Hello %s, your credit report has been generated below\n", customer_name);
+	printf("account number: %ld\n", accnt_num);
+	printf("%25s %25s\n", "New Balance", "Credit Remaining");
+	printf("%25.2Lf %25.2Lf\n", new_balance, credit_remaining);
 
+	//return with no error code
 	return 0;
 }
