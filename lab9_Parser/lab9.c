fa5a3ef1049d617907356529859f34acea0e0613
root <root@HP-LAPTOP.localdomain>
2018-10-02 11:01:40 -0700

    Finished lab 9

--- lab9_Parser/lab9.c
+++ lab9_Parser/lab9.c
@@ -17,26 +17,38 @@
 				* int input
 		II. 
 			a. Send input to int quotient and remainder functions
-			b. return output
+			b. return calculated values and store in indexes / input
 		III.
-			a. print intput spaced by 2 
+			a. print indexes spaced by 2 
 */
 
 #include <stdio.h>
 
-int int_quotient(int a, int b);
-int int_remainder(int a, int b);
+int tokenizer(int num, int dividend);
+int modder(int num, int dividend);
 
-int int_quotient(int a, int b)
+///*
+//takes in an integer and truncates the number by the dividend
+///*
+int tokenizer(int num, int dividend)
 {
 	int quotient;
+
+	//integer division
+	quotient = num / dividend;
 
 	return quotient;
 }
 
-int int_remainder(int a, int b)
+///*
+//takes in an integer and invert truncates the number by the dividend using mod
+///*
+int modder(int num, int dividend)
 {
 	int remainder;
+
+	//mod remainder
+	remainder = num % dividend;
 
 	return remainder;
 }
@@ -46,13 +58,27 @@ int main()
 	int input;
 	int index0, index1, index2, index3, index4;
 
+	//I.
 	puts("Welcome to the l337 parser");
 	puts("Please enter a postive integer (1-32767)");
 	scanf("%d", &input);
 
+	//test cases = 4652 works   06205 works  94639 works
+	//II.
+	index0 = tokenizer(input, 10000);
+	input = modder(input, 10000);
+	index1 = tokenizer(input, 1000);
+	input = modder(input, 1000);
+	index2 = tokenizer(input, 100);
+	input = modder(input, 100);
+	index3 = tokenizer(input, 10);
+	input = modder(input, 10);
+	index4 = tokenizer(input, 1);
 
+	//III.
+	puts("Below lies the parsed number: ");
+	printf("number --> %d  %d  %d  %d  %d\n", index0, index1, index2, index3, index4);
 
-	printf("\n%d %d %d %d %d\n", index0, index1, index2, index3, index4);
-
+	//return with no error code
 	return 0;
 }
