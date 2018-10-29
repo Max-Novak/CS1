aff3c5208e68f8645f8d88ea1c197bbe7bccd919
MaximusNovak <maximusnovakmn@gmail.com>
2018-10-29 10:29:43 -0700

    finished lab14

--- lab14_Multiple_Guess/lab14.c
+++ lab14_Multiple_Guess/lab14.c
@@ -1,6 +1,6 @@
 /*
 	Author: Max Novak
-	Date: 10/20/2018
+	Date: 10/29/2018
 	CS 1 Lab 14 - Multiple_Guess
 	Pseudo Code
 	Phase 1:
@@ -48,64 +48,121 @@ int main()
 	return 0;
 }
 
+/**********************************************
+* Determine if the user answered correctly    *
+**********************************************/
 int get_answer_math_library()
 {
 	//declarations
-	int a;
+	int answer;
+	int score = 0;
 
 	puts("What is the include statement to add the built in C library capable of providing mathematical functions such as sqrt?");
 	puts("(1) #include math;");
 	puts("(2) #include <mathlibrary.h>;");
 	puts("(3) #include <math.h>;");
-	puts("(4) #include <math>;")
+	puts("(4) #include <math>;");
 
 	//Take user input
-	scanf("%d", a);
+	scanf("%d", &answer);
 
 	//check answer to see if correct library inclusion statement was written
-	switch(a)
+	switch(answer)
 	{
 		case 1:
+			puts("Incorrect, header file inclusions must be enclosed in <>");
+			break;
+		case 2:
+			puts("Incorrect, mathlibrary is not a builtin library for providing math functions");
+			break;
+		case 3:
+			puts("Correct!");
+			score++;
+			break;
+		case 4:
+			puts("Incorrect, inclusions require the addition of a .h file extension");
 			break;
 	}
 
-	return test;
+	return score;
 }
 
+/**********************************************
+* Determine if the user answered correctly    *
+**********************************************/
 int get_answer_return_function()
 {
 	//Declarations
-	char answer[16];
-	int test = 1;
+	int answer;
+	int score = 0;
 
+	//check answer to see if correct num of returns is input
 	puts("How many data types can a single function call return?");
-	gets(answer);
+	puts("(1)");
+	puts("(2)");
+	puts("(3)");
+	puts("(4)");
+
+	//Take user input
+	scanf("%d", &answer);
 
 	//check answer to see if correct boolean value was input
-	if(strcmp(answer, "true") != 0)
+	switch(answer)
 	{
-		test = 0;
-		puts("Incorrect: The answer is \"true\" because functions by definition must only have 1 return value per each unique input");
+		case 1:
+			puts("Correct!");
+			score++;
+			break;
+		case 2:
+			puts("Incorrect, only 1 data type can be returned in a single function return");
+			break;
+		case 3:
+			puts("Incorrect, only 1 data type can be returned in a single function return");
+			break;
+		case 4:
+			puts("Incorrect, only 1 data type can be returned in a single function return");
+			break;
 	}
 
-	return test;
+	return score;
 }
 
+/**********************************************
+* Determine if the user answered correctly    *
+**********************************************/
 int get_answer_format_double()
 {
 	//Declarations
-	char answer[16];
-	int test = 1;
+	int answer;
+	int score = 0;
 
+	//ask the user which format specifier is correct
 	puts("What is the format specifier for a long double?");
-	gets(answer);
+	puts("(1) %d");
+	puts("(2) %ldz");
+	puts("(3) %u");
+	puts("(4) $Lf");
 
-	//check answer to see if format specifer was inputted
-	if(strcmp(answer, "%Lf") != 0)
+	//Take user input
+	scanf("%d", &answer);
+
+	//check answer to see if correct boolean value was input
+	switch(answer)
 	{
-		test = 0;
-		puts("Incorrect: The answer is %Lf");
+		case 1:
+			puts("Incorrect, that is the signed int format specifier");
+			break;
+		case 2:
+			puts("Incorrect, that is not a valid format specifier");
+			break;
+		case 3:
+			puts("Incorrect, that is the unsigned int format specifer");
+			break;
+		case 4:
+			puts("Correct!");
+			score++;
+			break;
 	}
 
-	return test;
+	return score;
 }
